896dba7d90196acf6b60620e11484760f7453a76
Vishal Pathak <yoyovishalpathak2004@gmail.com>
2023-10-21 13:20:38 +0530

    Fixed distance calc

--- android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
+++ android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
@@ -268,19 +268,10 @@ public class FlutterFirebaseMessagingReceiver extends BroadcastReceiver {
 
   public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
     // Convert latitude and longitude from degrees to radians
-    double lat1Rad = Math.toRadians(lat1);
-    double lon1Rad = Math.toRadians(lon1);
-    double lat2Rad = Math.toRadians(lat2);
-    double lon2Rad = Math.toRadians(lon2);
-
-    // Haversine formula
-    double dLat = lat2Rad - lat1Rad;
-    double dLon = lon2Rad - lon1Rad;
-
-    double a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.pow(Math.sin(dLon / 2), 2);
+    double dLat = Math.toRadians(lat2 - lat1);
+    double dLon = Math.toRadians(lon2 - lon1);
+    double a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.pow(Math.sin(dLon / 2), 2);
     double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    // Calculate the distance
     double distance = RADIUS_OF_EARTH * c;
     DecimalFormat df = new DecimalFormat("#.#");
     distance = Double.parseDouble(df.format(distance));
